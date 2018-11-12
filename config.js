0465ed15383f71f411b01c765f052e142cc71595
Elliott Alexander <escottalexander@gmail.com>
2018-11-12 13:29:21 -0500

    changed api token

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ const env = process.env.NODE_ENV || 'development'
 const DATABASE_URL = (
     process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    'postgres://localhost/dev-restaurants-app'
+    'postgres://localhost/house-flip-tools-db'
 );
 
 const TEST_DATABASE_URL = (
     process.env.TEST_DATABASE_URL ||
     global.TEST_DATABASE_URL ||
-    'postgres://localhost/test-restaurants-app');
+    'postgres://localhost/house-flip-tools-test-db');
 
 module.exports = {
     PORT: process.env.PORT || 8080,
